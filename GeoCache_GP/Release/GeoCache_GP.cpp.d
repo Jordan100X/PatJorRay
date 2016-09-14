f7fdd40a9d9dce97f4266b80ab22ba86dcfdee94
Raymond <RTLewandowski@fullsail.edu>
2016-09-13 22:26:32 -0400

    "Fixed math code"

--- GeoCache_GP/Release/GeoCache_GP.cpp.d
+++ GeoCache_GP/Release/GeoCache_GP.cpp.d
@@ -1,25 +1,25 @@
-C:\Users\Jordan\AppData\Local\Temp\VMicroBuilds\GeoCache_GP\uno\GeoCache_GP.cpp.o: \
- C:\Users\Jordan\AppData\Local\Temp\VMicroBuilds\GeoCache_GP\uno\GeoCache_GP.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Adafruit_NeoPixel/Adafruit_NeoPixel.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h
+C:\Users\UC-PC\AppData\Local\Temp\VMicroBuilds\GeoCache_GP\uno\GeoCache_GP.cpp.o: \
+ C:\Users\UC-PC\AppData\Local\Temp\VMicroBuilds\GeoCache_GP\uno\GeoCache_GP.cpp \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\PF\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ D:\PF\Arduino\libraries\SD\src/SD.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\PF\Arduino\libraries\SD\src/utility/SdFat.h \
+ D:\PF\Arduino\libraries\SD\src/utility/Sd2Card.h \
+ D:\PF\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
+ D:\PF\Arduino\libraries\SD\src/utility/SdInfo.h \
+ D:\PF\Arduino\libraries\SD\src/utility/FatStructs.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\PF\Arduino\libraries\SD\src/utility/SdFatUtil.h \
+ D:\PF\Arduino\libraries\Adafruit_NeoPixel/Adafruit_NeoPixel.h \
+ D:\PF\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ D:\PF\Arduino\hardware\arduino\avr\cores\arduino/Stream.h
